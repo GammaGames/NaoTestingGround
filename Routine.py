2a478f6288774de66b27aa4b235d1a9cc315ab04
Logan Warner <loganswarnerprofessional@gmail.com>
2016-12-16 10:33:55 -0700

    settrace-based automatic step increementer and poison-pill-chekcer

--- Routine.py
+++ Routine.py
@@ -10,6 +10,11 @@
 # DESCRIPTION: Routine superclass for routines to run from the GUI
 # ****************************************************************
 
+# --------------
+# Python imports
+# --------------
+import sys
+
 # -------------------
 # Application imports
 # -------------------
@@ -20,27 +25,39 @@ class Routine(object):
     
     '''
     __metaclass__ = ABCMeta
-    
+
     progressVar = None
-    
-    numberSteps = -1    
+
+    numberSteps = -1
     currentStep = 0
     running = False
-        
+
+    firstFrame = None
     def __init__(self):
         '''
         Default constructor
         '''
     #def __init__
-    
+
     @abstractmethod
     def connect(self, ip, port):
         return
     #def connect
-        
+
     @abstractmethod
-    def run(self):
+    def perform(self):
         return
+    #perform
+
+    def run(self):
+        self.running = True
+        try:
+            with SetTrace(self.monitor):
+                self.perform()
+        except StopIteration:
+            pass
+        finally:
+            self.running = False
     #def run
     
     def stop(self):
@@ -50,5 +67,32 @@ class Routine(object):
     def getPercent(self):
         return self.currentStep / float(self.numberSteps) * 100
     #def getPercent
-    
+
+    def monitor(self, frame, event, arg):
+        if event == "line":
+            if not self.firstFrame:
+                self.firstFrame = frame
+            if frame == self.firstFrame:
+                if not self.running:
+                    raise StopIteration
+                self.currentStep += 1
+        return self.monitor
+    #monitor
 #class Routine
+
+class SetTrace(object):
+    def __init__(self, func):
+        self.func = func
+    #__init__
+
+    def __enter__(self):
+        sys.settrace(self.func)
+        return self
+    #__enter__
+
+    def __exit__(self, ext_type, exc_value, traceback):
+        sys.settrace(None)
+    #__exit__
+#class SetTrace
+
+
